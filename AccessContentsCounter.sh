2c7595f12486788cd2ac9349b49b1b14e2baf3f6
ekazyam <readsnow77@gmail.com>
2015-05-10 23:38:14 +0900

    プレーンテキストに対しマルチスレッドで処理を高速化

--- AccessContentsCounter.sh
+++ AccessContentsCounter.sh
@@ -2,9 +2,9 @@
 ################################
 # Author: Rum Coke
 # Data  : 2015/05/10
-# Ver   : 0.2.0
+# Ver   : 0.3.0
 ################################
-# Contents Count For Text Data.
+# Contents Count For Zip Data.
 function ContentCount()
 {
 	# Set File Kind.
@@ -17,9 +17,15 @@ function ContentCount()
 		TARGET_FILE=${CONTENTS[$Y]}
 		
 		# Search Target File and Access Count.
-		test ${TARGET_KIND[0]} == ${KIND} && echo -e `zgrep "${TARGET_FILE}" "${TARGET_LOG}" | wc -l`"\t${TARGET_FILE}" | tee -a ${RESULT_LOG} 
-		test ${TARGET_KIND[1]} == ${KIND} && echo -e `grep "${TARGET_FILE}" "${TARGET_LOG}" | wc -l`"\t${TARGET_FILE}" | tee -a ${RESULT_LOG}
+		echo -e `zgrep "${TARGET_FILE}" "${TARGET_LOG}" | wc -l`"\t${TARGET_FILE}" | tee -a ${RESULT_LOG} 
 	done
+}
+
+# Contents Count For Text Data.
+function ContentCountFast()
+{
+	TARGET_FILE=${1}
+	echo -e `grep "${TARGET_FILE}" "${TARGET_LOG}" | wc -l`"\t${TARGET_FILE}" | tee -a ${RESULT_LOG}
 }
 
 # Log Kind Setting.
@@ -44,23 +50,11 @@ function SetContents()
 	test ${TARGET_KIND[1]} == ${KIND} && CONTENTS=( `cat ${TARGET_LOG} | awk -F' ' '{ print $7 }' | sed -r 's/(\=|\?).*$//g' | sort | uniq` )
 }
 
-##################
-# Main Function
-##################
-# Access Log Dir.
-LOG_DIR='/var/log/nginx/work'
-
-# Access Log.
-LOGS=(`ls ${LOG_DIR}/*access.log*`)
-
-# Work Directory.
-WORK_DIR='/tmp'
-
-# Supported Target File.
-TARGET_KIND=("Zip" "ASCII")
-
-for (( Z = 0; Z < ${#LOGS[@]}; ++Z ))
-do
+# Search Start.
+# linear: zip
+# multi : ascii
+function SearchStart()
+{
 	# Set Target Log.
 	TARGET_LOG=${LOGS[$Z]}
 
@@ -77,9 +71,43 @@ do
 			# Set Contents
 			SetContents ${LOG_KIND}
 
-			# Contents Count.
-			ContentCount ${LOG_KIND}
+			if [ ${TARGET_KIND[0]} == ${KIND} ]
+			then
+				# Contents Count for zip.
+				ContentCount ${LOG_KIND}
+			else
+				# Contents Count for text.
+				echo ${CONTENTS[@]} | sed 's/ /\n/g' | xargs -P${MAX_P} -n1 -I % bash -c "ContentCountFast %"
+			fi
+			
 		fi
 	done
-done
+}
 
+##################
+# Main Function
+##################
+# Access Log Dir.
+LOG_DIR='/var/log/nginx/work'
+
+# Access Log.
+LOGS=(`ls ${LOG_DIR}/*access.log*`)
+
+# Work Directory.
+WORK_DIR='/tmp'
+
+# Supported Target File.
+TARGET_KIND=("Zip" "ASCII")
+
+# Max Process.
+MAX_P=4
+
+# Export
+export -f SetContents SetResultLog SetLogKind ContentCount ContentCountFast
+export WORK_DIR TARGET_KIND MAX_P TARGET_LOG RESULT_LOG
+
+for (( Z = 0; Z < ${#LOGS[@]}; ++Z ))
+do
+	# Start.
+	SearchStart
+done
