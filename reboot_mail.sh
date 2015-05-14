05c6567439bb4b97b2e69da9201d7197c4cf8eb3
rumcoke <ekazyam@users.noreply.github.com>
2015-05-14 19:11:48 +0900

    件名に日本語を利用可能とした

--- reboot_mail.sh
+++ reboot_mail.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
 
 # Create mail header.
-function CreateMailHeader()
+function CreateMail()
 {
-	echo ${From}
-	echo ${To}
-	echo ${Subject}
+	echo ${Header_From}
+	echo ${Header_To}
+	echo ${Header_Subject}
 	echo
-	echo ${Header}
 	echo ${Message}
 	echo ${Hostname}
 	echo ${Date}
 }
 
-# Set mail parameter.
-function SetParameter()
+# Define parameter.
+function DefineParameter()
 {
-	# Mail Header at From and To Field.
-	From='From:hoge@fuga.co.jp'
-	To='To:piyo@fuga.co.jp'
-
-	# Mail Header at Message Field.
-	Subject='Subject:[Attention] Server Rebooted.'
-	Header='--------------------'
+	# Mail Info.
+	From='test@test.co.jp'
+	To='hogehoge@co.jp'
+	Subject='[通知]サーバー再起動'
+	Message='以下のサーバがリブートしました。'
 	Hostname='ホスト名：'`uname -n`
 	Date='日付：'`date`
-	Message='以下のサーバがリブートしました。'
+}
+
+# Set mail Header.
+function SetHeader()
+{
+	# Mail Header.
+	Header_From='From:'${From}
+	Header_To='To:'${To}
+	Header_Subject=`echo ${Subject} | base64 | tr -d \n`
+	Header_Subject='Subject:=?utf-8?B?'"${Header_Subject}"'?='
 }
 
 #################
 # Main function.
 #################
-# Usage:
-# crontab -e
-# @reboot /bin/bash /root/reboot_mail.sh
-# 
+# Define Parameter
+DefineParameter
 
-# Set Parameter for Mail Header.
-SetParameter
+# Set Mail Header.
+SetHeader
 
 # Create Mail Data and Send mail.
-CreateMailHeader | /usr/sbin/sendmail -i -t
+CreateMail | /usr/sbin/sendmail -i -t
 
