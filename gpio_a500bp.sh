446dfcaed7ccaf126a0f802b9d3ba21d3bcfb807
ekazyam <readsnow77@gmail.com>
2015-06-20 23:32:50 +0900

    ソースを整形、改修難度を容易化

--- gpio_a500bp.sh
+++ gpio_a500bp.sh
@@ -2,7 +2,7 @@
 ################################
 # Author: Rum Coke
 # Data  : 2015/06/21
-# Ver   : 0.9.0
+# Ver   : 0.9.1
 ################################
 function initGpio()
 {
@@ -18,15 +18,29 @@ function checkGpio()
 {
 	while true
 	do
-		if [ `cat ${TARGET_DIR}/value` == 0 ]
+		if [ `cat ${TARGET_DIR}/value` == ${ON} ]
+		then
 			# Sensor On.
 			echo "ON."
+			
+			# Execute.
+			executeCmd
 		else
 			# Sensor Off.
 			echo "OFF."
 		fi
+
+		# Wait.
 		sleep ${TIME}
 	done	
+}
+
+function executeCmd()
+{
+	echo "Execute!"
+
+	# Wait.
+	sleep ${TIME_CMD}
 }
 
 #####################
@@ -36,14 +50,21 @@ function checkGpio()
 GPIO=25
 
 # Wait Time.
-TIME=1s
+TIME=10s
+
+# Wait for Cmd.
+TIME_CMD=5s
 
 # Target Directory.
 TARGET_DIR="/sys/class/gpio/gpio${GPIO}"
 
+# On
+ON=1
 
 # Init Gpio.
 initGpio
 
 # Check Gpio.
 checkGpio
+
+# End Function. #
