ce784f0b43fe5770c509bba26e20839cb6eb5f90
ekazyam <readsnow77@gmail.com>
2015-07-11 12:50:43 +0900

    新規リポジトリに移設

--- munin_template_selecter/backend/cmd_exe.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_POST['munin_radio']))
-{
-	$target = str_replace('static_' , '' , $_POST['munin_radio'] );
-	$command = 'tools/changetemplate.sh ' . $target;
-	exec($command);
-}
-?>
